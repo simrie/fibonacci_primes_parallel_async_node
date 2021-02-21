9e8aea3e9265d8fd84c933637445e89b6b198820
susanimrie <susanimrie@hotmail.com>
2021-02-21 00:54:22 -0500

    One fibonacci with reducer

--- index.js
+++ index.js
@@ -7,75 +7,76 @@
 */
 
 
-//const _ = require('lodash');
 //const fs = require('fs');
-const isInteger = require("lodash").isInteger;
-const isPrime = require("is-prime-value");
+const { toNumber, isInteger, reducer } = require("lodash");
 const events = require("events");
-const format = require("util").format;
-const readline = require("readline");
+const { format } = require("util");
 const { exit } = require("process");
+const isPrime = require("is-prime-value");
+const readline = require("readline");
 
-const { event } = events;
 
 let prime_count = 0;
 let fib_count = 0;
-let N = 7;
-let capture = "";
+let N = 0;
+let capture = [];
 let output = "";
 
 
 const eventEmitter = new events.EventEmitter();
 
-const processingCompletedHandler = function() {
-    output="Processing is completed.";
-    capture = format(`%s\n%s`, output, capture);
-    console.log(output);
-}
 
 const primeNumberHandler = function (fib_sum) {
-    if (isNaN(fib_sum)) {
-        return;
-    }
     if (prime_count > N) {
-        return;
+        //return;
     }
     if (isPrime(fib_sum)) {
         prime_count++;
         output = format(`PRIME %d: %d`, prime_count, fib_sum);
-        capture = format(`%s\n%s`, output, capture);
-        console.log(output);
+        capture.push(output);
     }
 }
 
 const fibNumberHandler = function (fib_sum) {
-    if (isNaN(fib_sum)) {
-        return;
-    }
     if (fib_count > N) {
-        return;
+        //return;
     }
     fib_count++;
     output=format(`FIB %d: %d`, fib_count, fib_sum);
-    capture = format(`%s\n%s`, output, capture);
-    console.log(output);
+    capture.push(output);
+}
+
+const fibonacci = function(n) {
+    if (n === 1) {
+        return 1;
+    } else if (n < 1) {
+        return "OOPS";
+    }
+
+    let array = new Array(n);                              // <---- Starting here
+    let filled = array.fill(1);
+    let reduced = filled.reduce((acc, _, i) => {
+        let res = (i <= 1) ? i : acc[i-2] + acc[i-1];
+        acc.push(res);
+        eventEmitter.emit("fib_sum", res);
+        return acc;                          
+    },[]); // <- reduce is initialized with an array (new array),
+    const sum_of_last_two_n = reduced[n - 1] + reduced[n - 2];
+    eventEmitter.emit("fib_sum", sum_of_last_two_n);
+    return sum_of_last_two_n;
 }
 
 
 eventEmitter.addListener('fib_sum', fibNumberHandler);
 eventEmitter.addListener('fib_sum', primeNumberHandler);
-eventEmitter.addListener('done', processingCompletedHandler);
-
-eventEmitter.emit("fib_sum", 5);
-eventEmitter.emit("fib_sum", 6);
-eventEmitter.emit("fib_sum", "fish");
 
 const readline_inteface = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-})
+});
 
 readline_inteface.question(`Until I reach what integer should I process Fibonacci numbers? `, N => {
+    N = toNumber(N);
     if (isNaN(N)) {
         console.log(`Your input is not a number. Quitting.`)
         readline_inteface.close()
@@ -83,12 +84,13 @@ readline_inteface.question(`Until I reach what integer should I process Fibonacc
     }
 
     if (!isInteger(N)) {
-        
         console.log(`Your input is not an integer. Quitting.`)
         readline_inteface.close()
         exit(0);
     }
     console.log(`Processing Fibonacci numbers up to ${N}.  Prime numbers will be noted.`)
     readline_inteface.close()
-    exit(0);
+    fibonacci(N);
+    capture = capture.join("\n");
+    console.log(capture);
 })
